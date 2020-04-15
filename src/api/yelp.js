ebbff2f8ba2fbe1feb31aa07e9fc3a32019378e8
Mike Phan <khoaphan90@gmail.com>
2020-04-15 15:34:26 -0400

    Remove key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses/',
     headers: {
-        Authorization: 'Bearer DJonjKzB8UmeTdFlt-kebzNR20wKhuIetH3VAazV3Pr17n6iIaVupw7Xj9xn9YFc6Y66tiYUThSrYHyL7n6-fQbXsAGHIstDrkln6xJUyqMrrOEaX41j9JJlcBB5XnYx' 
+        Authorization: 'Bearer <>' 
     }
 });
